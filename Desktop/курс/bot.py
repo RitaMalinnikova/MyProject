8ccd4d35e2a8d92d2dfa44487fafdaf0969e22ca
RitaMalinnikova <rita_malina@bk.ru>
2021-06-04 20:48:23 +0300

    тут коммент

---
+++ Desktop/курс/bot.py
@@ -0,0 +1,13 @@
+import requests 
+import datetime
+
+Class Weather(self):
+    def __init__(self, token):
+        self.token = token
+        self.api_url = ''.
+
+    def get_weather(self,city):
+        response
+        
+if __name__ == '__main__':
+    w = Weather()
